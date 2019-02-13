7700c091e9e52959115774d253955b488642318e
Moses Olawoyin <m.bastford@gmail.com>
2019-02-13 01:55:58 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,6 +5,7 @@ def reformat_languages(languages)
     language.each do|name, language_details|
       language_details.each do|type, values|
        binding.pry
+       if new_hash[name].nil?
       new_hash[name] = {}
       
        end
